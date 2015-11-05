1deb2cfd981f0f59b9dd56bc25ddc69fad9d5e72
alisa colon <alisaclone@gmail.com>
2015-11-05 15:39:34 -0800

    add integration spec;

---
+++ spec/features/add_items_page_spec.rb
@@ -0,0 +1,14 @@
+# require 'rails_helper'
+#
+#   describe 'the items path' do
+#
+#     it 'can add a new item', js: true do
+#       visit items_path
+#       click_on 'add item'
+#       fill_in 'item_name', with: 'eggs'
+#       fill_in 'item_quantity', with: '12'
+#       fill_in 'item_description', with: 'Come and get er'
+#       click_on 'Submit'
+#       expect(page).to have_content 'eggs'
+#     end
+#   end
